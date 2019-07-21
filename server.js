d50ef1395d4f2a9cc842432759c15ad0d9d91b65
ryverine <rwcheek@gmail.com>
2019-07-20 20:37:04 -0400

    adding MONGODB_URI for db connection

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 var db = require("./models");
-var PORT = 3000;
+// var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var app = express();
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -16,7 +17,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/rNewsScraper", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/rNewsScraper";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// mongoose.connect("mongodb://localhost/rNewsScraper", { useNewUrlParser: true });
 
 app.get("/", function(req, res) 
 {
